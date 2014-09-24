86f36afeaa7755dbc8172dd3572d5b6362d7c3bb
awalin sopan <awalinnabila@gmail.com>
2014-09-24 12:57:48 -0400

    translation and scale done, need to adjust Y-values with scaling, too. Then add the Y-axis labels. Also, added Commit animation code to disable the implicit animation to remove the jittering effect.

--- UsehrIPAD/UsherTimeseriesViz.m
+++ UsehrIPAD/UsherTimeseriesViz.m
@@ -53,18 +53,12 @@
     if(!visibleKeys){
         return;
     }
-    
-    
-    //SOPAN:
     NSLog(@"draw layers");
-    //    NSLog(@"%@",dataPoints);
-    //Drawing starts
-    height = rect.size.height;
-    eachWidth = rect.size.width/([dataPoints count] +1);
-    
-    //     NSLog(@"visible keys %d", count);
-    
-    //     NSLog(@"max = %d, each width %f ", [max intValue], eachWidth);
+//    [self drawShapeLayer];
+}
+
+-(void) drawShapeLayer{
+
     int i = 0;
     float x=0.0;
     for(id ky in [[dataPoints allKeys] sortedArrayUsingSelector: @selector(compare:)]){
@@ -91,10 +85,8 @@
     }
     [chart closePath];
     shapeLayer.path = chart.CGPath;
-    
+
 }
-
-
 
 
 //first time creation of the data  for the chart
@@ -106,7 +98,7 @@
     [shapeLayer setBounds:CGRectMake(0.0, 0.0, [self frame].size.width, [self frame].size.height )];
     [shapeLayer setPosition: CGPointMake(self.frame.size.width/2,
                                          self.frame.size.height/2)];
-    
+   
     //    UIColor * darkColor = [UIColor colorWithRed:1.0/255.0 green:1.0/255.0 blue:67.0/255.0 alpha:1];
     UIColor * lightColor = [UIColor colorWithRed:3.0/255.0 green:3.0/255.0 blue:90.0/255.0 alpha:0.6];
     shapeLayer.fillColor = lightColor.CGColor;
@@ -128,16 +120,19 @@
     
     self.layer.masksToBounds = YES;
     
+    height = self.layer.frame.size.height;
+    eachWidth = self.layer.frame.size.width/([dataPoints count] +1);
+    
+    
     max = [[[dataPoints allValues] valueForKeyPath: @"@max.self"] floatValue];
     [self.layer addSublayer:shapeLayer];
-    
-    [self setNeedsDisplay];
+    [self drawShapeLayer];
+//    [self setNeedsDisplay];
 }
 
+
 -(void) adjustAnchor:(CGPoint) anchor {
-    
     NSLog(@"adjusting anchor");
-    
     shapeLayer.anchorPoint = CGPointMake(anchor.x / shapeLayer.bounds.size.width, shapeLayer.anchorPoint.y);
     
 }
@@ -148,73 +143,58 @@
     zooming = YES;
     scale = scalez;
     NSRange theRange;
-       NSLog(@"scale %f", scale);
-    
+    NSLog(@"scale %f", scale);
+//    eachWidth *=scale;
     //Center the zooming around the pinch point
     
     int visiblePoints = (int)(self.layer.frame.size.width/(eachWidth*scale));
     NSLog(@"visible points %d", visiblePoints);
-    if(visiblePoints<3){
-        return;
-    }
-    else if(visiblePoints <= [dataPoints count]){
+    
+    if(visiblePoints <= [dataPoints count]){
         theRange.length = (int)(roundf(visiblePoints-0.5))-1; // considering scale = 2
         //        NSLog(@"%d, %d", theRange.length, [visibleKeys count]);
         int midvalue = (int)(roundf(center.x/(eachWidth*scale)-0.5));
        
-        theRange.location = midvalue-theRange.length/2;
-        NSLog(@"%d, %d", theRange.length, theRange.location);
+        theRange.location = MAX(0, (int)(midvalue-theRange.length/2));
+        int last = MIN([dataPoints count]-1, theRange.location+theRange.length);
+        theRange.length= last-theRange.location;
         
-
+        NSLog(@"%d, %d,  %d", theRange.length, midvalue, theRange.location);
+        
         visibleKeys = (NSMutableArray*)[sortedKeys subarrayWithRange:theRange ] ;
         
-        NSArray* exisitngpoints= [dataPoints objectsForKeys:visibleKeys notFoundMarker:@"0"] ;
-        NSLog(@"%@", exisitngpoints);
-        if([exisitngpoints count]<3){
-            NSLog(@"Too few markers");
-            return;
-        }
-        
         CABasicAnimation *animatePath = [CABasicAnimation animationWithKeyPath:@"transform"];
-        [animatePath setFromValue: [NSValue valueWithCATransform3D:CATransform3DScale(shapeLayer.transform, 1.0, 1.0, 1.0) ]];
+        [animatePath setFromValue: [NSValue valueWithCATransform3D:shapeLayer.transform]];
         [animatePath setToValue:   [NSValue valueWithCATransform3D:CATransform3DScale(shapeLayer.transform, scale, 1.0, 1.0) ]];
-        [animatePath setDuration:1.0];
-        
-        [shapeLayer addAnimation:animatePath forKey:nil];
+        [animatePath setDuration:0.25];
+        [CATransaction begin]; // this removes the jittering effect
+        [CATransaction setDisableActions:YES];
         shapeLayer.transform = CATransform3DScale(shapeLayer.transform, scale, 1.0, 1.0);
+        [CATransaction commit];
+        [shapeLayer addAnimation:animatePath forKey:nil];
         NSLog(@"zooming");
-        
 
-        
-    //    [self setNeedsDisplay];
         } else {
-        NSLog(@"doing nothing %d, scale %f ", visiblePoints, scalez );
-        scalez = 1.0;
-        return;
+            NSLog(@"doing nothing %d, scale %f ", visiblePoints, scalez );
+            scalez = 1.0;
+            return;
     }
 //
-    
-    
-    
     
 }
 
 
 -(void) panView:(CGPoint) translate{
     NSLog(@"translating view ");
-    
-    //    CGPoint center = [shapeLayer position];
-    //    NSLog(@"%f %f, trans %f , %f ", shapeLayer.position.x, shapeLayer.position.y, translate.x, translate.y );
-    //    [shapeLayer setPosition:CGPointMake(center.x+translate.x*0.5, center.y)];
-    
-    CABasicAnimation *animatePath = [CABasicAnimation animationWithKeyPath:@"transform.translate"];
-    [animatePath setFromValue: [NSValue valueWithCATransform3D:CATransform3DTranslate(shapeLayer.transform, 0.0, 0.0, 0.0) ]];
+    CABasicAnimation *animatePath = [CABasicAnimation animationWithKeyPath:@"transform"];
+    [animatePath setFromValue: [NSValue valueWithCATransform3D:shapeLayer.transform]];//identity or shapeLayer.transform
     [animatePath setToValue:   [NSValue valueWithCATransform3D:CATransform3DTranslate(shapeLayer.transform, translate.x*0.2, 0.0, 0.0) ]];
     [animatePath setDuration:0.2];
-    
-    [shapeLayer addAnimation:animatePath forKey:nil];
+    [CATransaction begin];
+    [CATransaction setDisableActions:YES];
     shapeLayer.transform = CATransform3DTranslate(shapeLayer.transform, translate.x*0.2, 0.0, 0.0);
-    
+    [CATransaction commit];
+    [shapeLayer addAnimation:animatePath forKey:nil];
     
 }
 
@@ -222,10 +202,8 @@
 
 //TODO:need to call it after panning, too
 -(void) adjustYscale {
-    return;
-    
+    //return;
     // now change the y-axis with animation
-    
     float cmax = [[[dataPoints objectsForKeys:visibleKeys notFoundMarker:@"0"] valueForKeyPath: @"@max.self"] floatValue];
     if(cmax!=max){
         
